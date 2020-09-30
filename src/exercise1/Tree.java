dcbb2740ddf94302354ff92893e8da6702647b26
mariatod62 <maria.todorova@fdiba.tu-sofia.bg>
2020-09-30 16:16:04 +0000

    -

--- src/exercise1/Tree.java
+++ src/exercise1/Tree.java
@@ -2,11 +2,11 @@ package exercise1;
 
 public class Tree {
   public static void main (String [] args) {
-    System.out.print("     *\n");
-    System.out.print("    ***\n");
-    System.out.print("   *****\n");
-    System.out.print("  *******\n");
-    System.out.print(" *********\n");
-    System.out.print("    ***\n");
+    System.out.print("    *\n");
+    System.out.print("   ***\n");
+    System.out.print("  *****\n");
+    System.out.print(" *******\n");
+    System.out.print("*********\n");
+    System.out.print("   ***\n");
   }
 }
